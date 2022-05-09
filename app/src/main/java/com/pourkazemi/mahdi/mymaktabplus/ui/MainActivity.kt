4cd9a4d94b4bda86f8257b82b2397c71c3d31a87
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-05-09 21:18:01 +0430

    splash screen implemented

--- app/src/main/java/com/pourkazemi/mahdi/mymaktabplus/ui/MainActivity.kt
+++ app/src/main/java/com/pourkazemi/mahdi/mymaktabplus/ui/MainActivity.kt
@@ -2,17 +2,23 @@ package com.pourkazemi.mahdi.mymaktabplus.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
 import com.pourkazemi.mahdi.mymaktabplus.R
 import com.pourkazemi.mahdi.mymaktabplus.data.localdetabase.data_store.PreferencesInfo
 import com.pourkazemi.mahdi.mymaktabplus.util.logger
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -36,7 +43,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.setting -> {
                 navController.navigate(R.id.settingFragment)
                 true
@@ -49,14 +56,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun preferencesInit() {
         lifecycleScope.launch {
-            viewModel.preferences.collect { info: PreferencesInfo ->
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val info = viewModel.preferences.first()
                 logger("theme: ${info.theme} , lang: ${info.lang}")
 
                 val mode = info.theme.mode
-                if (AppCompatDelegate.getDefaultNightMode() != mode) {
+                val currentMode = AppCompatDelegate.getDefaultNightMode()
+                Log.d("ahmadabadi", "preferencesInit: " + currentMode.toString())
+                if (currentMode != mode) {
                     AppCompatDelegate.setDefaultNightMode(mode)
                 }
             }
         }
+        // job.join()
     }
 }
